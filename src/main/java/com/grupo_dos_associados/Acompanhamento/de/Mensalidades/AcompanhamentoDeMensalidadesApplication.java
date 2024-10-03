02d417317dcd9fc225eb0d50c8ece266467880c3
erbert-gadelha <ebgr@cin.ufpe.br>
2024-10-02 23:37:08 -0300

    initial

---
+++ src/main/java/com/grupo_dos_associados/Acompanhamento/de/Mensalidades/AcompanhamentoDeMensalidadesApplication.java
@@ -0,0 +1,13 @@
+package com.grupo_dos_associados.Acompanhamento.de.Mensalidades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcompanhamentoDeMensalidadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcompanhamentoDeMensalidadesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo_dos_associados/Acompanhamento/de/Mensalidades/AcompanhamentoDeMensalidadesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo_dos_associados.Acompanhamento.de.Mensalidades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcompanhamentoDeMensalidadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
